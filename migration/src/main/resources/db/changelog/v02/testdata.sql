02cda17d95eeb84345392b0dc98a0e8aa7057bf7
laetansky <vlaetansky@gmail.com>
2023-10-18 15:48:42 +0300

    Update OrderStatus

--- migration/src/main/resources/db/changelog/v02/testdata.sql
+++ migration/src/main/resources/db/changelog/v02/testdata.sql
@@ -11,9 +11,9 @@ insert into couriers(phone, status, coordinates) values ('+79236425387', 'AVAILA
 insert into couriers(phone, status, coordinates) values ('+79014504912', 'BUSY','77.523, 88.324');
 insert into couriers(phone, status, coordinates) values ('+79126678006', 'BUSY','99.234, 11.123');
 
-insert into orders (customer_id, restaurant_id, status, courier_id, timestamp) values (1,1,'COMPLETE',1,'2023-04-11 15:05:00');
-insert into orders (customer_id, restaurant_id, status, courier_id, timestamp) values (3,2,'IN_PROCESS',2,'2023-05-12 15:10:00');
-insert into orders (customer_id, restaurant_id, status, courier_id, timestamp) values (3,3,'IN_DELIVERY',3,'2023-06-13 15:15:00');
+insert into orders (customer_id, restaurant_id, status, courier_id, timestamp) values (1,1,'CUSTOMER_CREATED',1,'2023-04-11 15:05:00');
+insert into orders (customer_id, restaurant_id, status, courier_id, timestamp) values (3,2,'DELIVERY_PENDING',2,'2023-05-12 15:10:00');
+insert into orders (customer_id, restaurant_id, status, courier_id, timestamp) values (3,3,'DELIVERY_PICKING',3,'2023-06-13 15:15:00');
 
 insert into restaurant_menu_items(restaurant_id, name, price, image, description) values (1,'menuitem1',200,'http://food/image1.jpg', 'description');
 insert into restaurant_menu_items(restaurant_id, name, price, image, description) values (2,'menuitem2',100,'http://food/image2.jpg', 'description');
