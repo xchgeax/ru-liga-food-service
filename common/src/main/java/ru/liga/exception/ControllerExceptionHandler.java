171a4ffce23bcc13a5cf2bd581fd00a6466775f3
laetansky <vlaetansky@gmail.com>
2023-10-31 13:03:49 +0300

    DeliveryService

--- common/src/main/java/ru/liga/exception/ControllerExceptionHandler.java
+++ common/src/main/java/ru/liga/exception/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.liga.entity.Order;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -16,5 +17,15 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(value = NoOrderItemsSuppliedException.class)
     protected ResponseEntity<Object> handleNoOrderItemsSuppliedException(NoOrderItemsSuppliedException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = IncorrectOrderStateException.class)
+    protected ResponseEntity<Object> handleIncorrectOrderStateException(IncorrectOrderStateException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = OrderAlreadyDeliveredException.class)
+    protected ResponseEntity<Object> handleOrderAlreadyDeliveredException(OrderAlreadyDeliveredException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 }
--- delivery-service/src/main/java/ru/liga/service/DeliveryService.java
+++ delivery-service/src/main/java/ru/liga/service/DeliveryService.java
@@ -7,6 +7,8 @@ import ru.liga.dto.DeliveryDto;
 import ru.liga.dto.OrderDto;
 import ru.liga.entity.Order;
 import ru.liga.entity.OrderStatus;
+import ru.liga.exception.IncorrectOrderStateException;
+import ru.liga.exception.OrderAlreadyDeliveredException;
 import ru.liga.exception.ResourceNotFoundException;
 import ru.liga.mapper.DeliveryMapper;
 import ru.liga.repo.OrderRepository;
@@ -31,6 +33,33 @@ public class DeliveryService {
         List<OrderDto> orders = ordersFeign.getOrderListByStatus(status);
 
         return deliveryMapper.orderDtoToDeliveryDto(orders);
+    }
+
+    public void completeDelivery(Long id) throws IncorrectOrderStateException, ResourceNotFoundException, OrderAlreadyDeliveredException {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Delivery not found"));
+
+        checkOrderIsCompleted(order);
+        if (!order.getStatus().equals(OrderStatus.DELIVERY_DELIVERING))
+            throw new IncorrectOrderStateException("Order is not yet picked up");
+
+        order.setStatus(OrderStatus.DELIVERY_COMPLETE);
+        orderRepository.save(order);
+    }
+
+    public void pickDelivery(Long id) throws ResourceNotFoundException, IncorrectOrderStateException, OrderAlreadyDeliveredException {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Delivery not found"));
+
+        checkOrderIsCompleted(order);
+        if (!order.getStatus().equals(OrderStatus.DELIVERY_PICKING))
+            throw new IncorrectOrderStateException("Order is not yet ready");
+
+        order.setStatus(OrderStatus.DELIVERY_DELIVERING);
+        orderRepository.save(order);
+    }
+
+    private void checkOrderIsCompleted(Order order) throws OrderAlreadyDeliveredException {
+        if (order.getStatus().equals(OrderStatus.DELIVERY_COMPLETE))
+            throw new OrderAlreadyDeliveredException();
     }
 
 }
