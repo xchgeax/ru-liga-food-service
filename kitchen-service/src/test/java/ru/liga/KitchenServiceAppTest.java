2062f4e22441ff27ac9e39c7d326febd5438a661
laetansky <vlaetansky@gmail.com>
2023-11-03 13:34:14 +0300

    RestaurantServiceTest

---
+++ kitchen-service/src/test/java/ru/liga/KitchenServiceAppTest.java
@@ -0,0 +1,11 @@
+package ru.liga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KitchenServiceAppTest {
+    @Test
+    void contextLoads() {
+    }
+}
