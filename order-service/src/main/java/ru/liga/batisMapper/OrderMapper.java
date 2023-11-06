1beaa0181fbb56297a971ef0fdf7dabd29410b94
laetansky <vlaetansky@gmail.com>
2023-11-06 11:42:29 +0300

    Move batis ordermapper

--- order-service/src/main/java/ru/liga/batisMapper/OrderMapper.java
+++ order-service/src/main/java/ru/liga/batisMapper/OrderMapper.java
@@ -1,9 +1,11 @@
 package ru.liga.batisMapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import ru.liga.entity.Order;
 
 import java.util.List;
 
+@Mapper
 public interface OrderMapper {
     Order getOrderById(int id);
 
